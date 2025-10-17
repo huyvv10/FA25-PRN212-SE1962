51b4c52e4003116ac97de5a3054af202fbe866f5
Vu Van Huy <huyvv10@fpt.edu.vn>
2025-10-17 16:28:27 +0700

    Commit

---
+++ ProductManagement/ProductManagement/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductManagement.Models;
+
+public partial class Product
+{
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Unit { get; set; } = null!;
+
+    public int Qty { get; set; }
+
+    public string CatId { get; set; } = null!;
+
+    public virtual Category Cat { get; set; } = null!;
+}
---
+++ ProductManagement/ProductManagement/Models/Se1962dbContext.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ProductManagement.Models;
+
+public partial class Se1962dbContext : DbContext
+{
+    public Se1962dbContext()
+    {
+    }
+
+    public Se1962dbContext(DbContextOptions<Se1962dbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(GetConnectionString());
+    }
+    private String GetConnectionString()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+        var strConn = config["ConnectionStrings:DefaultConnectionString"];
+
+        return strConn;
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CatId).HasName("PK__Category__6A1C8ADA4AEC4223");
+
+            entity.ToTable("Category");
+
+            entity.Property(e => e.CatId)
+                .HasMaxLength(4)
+                .IsUnicode(false)
+                .IsFixedLength()
+                .HasColumnName("CatID");
+            entity.Property(e => e.CatName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Product__3214EC071EC01D78");
+
+            entity.ToTable("Product");
+
+            entity.Property(e => e.Id)
+                .HasMaxLength(5)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.CatId)
+                .HasMaxLength(4)
+                .IsUnicode(false)
+                .IsFixedLength()
+                .HasColumnName("CatID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Unit)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Cat).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CatId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Product__CatID__38996AB5");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
